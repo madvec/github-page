38d73900dc93e3a7430bce0b661be506e5f1525e
alejandro__delgadillo <manuel.delgadillo@civir.mx>
2023-09-26 21:15:31 -0600

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,12 @@
-module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/github-page/' : '/' }
+// module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/github-page/' : '/' }
+
+// module.exports = {
+//     publicPath: "/github-page",
+// }
+
+module.exports = {
+    publicPath: "",
+    configureWebpack: {
+
+    }
+}
