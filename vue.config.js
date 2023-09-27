737e29b99c7e0c3452c38d26067ffde525368677
alejandro__delgadillo <manuel.delgadillo@civir.mx>
2023-09-26 21:27:11 -0600

    change vue config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/github-page' : '/' }
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    base:'/github-page/',
+    plugins:[vue()]
+})
+
+
+
+// module.exports = { publicPath: process.env.NODE_ENV === 'production' ? '/github-page' : '/' }
 
 // module.exports = {
 //     publicPath: "/github-page",
